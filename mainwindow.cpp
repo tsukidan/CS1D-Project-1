23b5c20aedf5de9c6fae4d06eae4cd8efdbd9c6e
TheBeesKnees111 <60633893+TheBeesKnees111@users.noreply.github.com>
2020-09-21 15:40:54 -0700

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,43 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+                /******************************
+                 **** SPECIFIC TO HOME TAB ****
+                 ******************************/
+
+    /************************************************************
+    * PROCESSING - Assign an image into new QPixmap variable
+    ************************************************************/
+    QPixmap homePageBackground (":/new/background/european-cities.jpg");
+
+    /************************************************************
+    * PROCESSING - Set the label to hold the image specified
+    ************************************************************/
+    int ImageLabelW =  ui -> ImageLabel -> width ();
+    int ImageLabelH =  ui -> ImageLabel -> height();
+    ui -> ImageLabel -> setPixmap(homePageBackground.scaled
+                      (ImageLabelW, ImageLabelH,
+                       Qt::KeepAspectRatio));
+
+    /************************************************************
+    * PROCESSING - Access the label and give values of:
+    *              Background = white
+    ************************************************************/
+    ui -> CustomerLabel -> setText("");
+    ui -> CustomerLabel -> setStyleSheet("QLabel { background-color : tan;"
+                                         "border: 1px solid black; }");
+
+    /************************************************************
+    * PROCESSING - Access the label and give values of:
+    *              Background = white
+    ************************************************************/
+    ui -> AdminLabel -> setText("");
+    ui -> AdminLabel -> setStyleSheet("QLabel { background-color : tan;"
+                                         "border: 1px solid black; }");
+
+
+
 }
 
 MainWindow::~MainWindow()
