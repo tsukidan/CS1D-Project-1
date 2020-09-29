58d41841a8e49527ff27c774b344abf6f597b674
Daniel <minatosakee@gmail.com>
2020-09-28 19:38:07 -0700

    test

--- main.cpp
+++ main.cpp
@@ -4,23 +4,19 @@
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
-=======
 
 
->>>>>>> a596f8d2cde4996e4819504bf506aae065b0a520
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();
-<<<<<<< HEAD
-=======
 
->>>>>>> a596f8d2cde4996e4819504bf506aae065b0a520
 
     SQLDatabase db;
     db.createDatabase();
     db.readFileCities();
     db.readFileDistances();
     db.readFileFoods();
+
+
 }
